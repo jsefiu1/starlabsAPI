6a3a11ba592b7b67d66ed0315aee875d6cd150fd
Drilon Sylejmani <drilon.sylejmani2@gmail.com>
2023-06-19 14:39:05 +0200

    small fixes

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,26 +1,19 @@
 import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
+from sqlalchemy.sql import text
 import yaml
-__all__ = ['test_db_connection']
+
+__all__ = ["test_db_connection"]
 
 load_dotenv()
 
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "joni")
-DB_NAME = os.getenv("DB_NAME", "fastapi")
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
+DB_PORT = os.getenv("POSTGRES_PORT", "5432")
+DB_USER = os.getenv("POSTGRES_USER", "postgres")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "joni")
+DB_NAME = os.getenv("POSTGRES_DB", "fastapi")
 
-with open("config.yaml") as file:
-    config = yaml.safe_load(file)
-
-db_settings = config["db"]
-DB_HOST = db_settings.get("host", DB_HOST)
-DB_PORT = db_settings.get("port", DB_PORT)
-DB_USER = db_settings.get("user", DB_USER)
-DB_PASSWORD = db_settings.get("password", DB_PASSWORD)
-DB_NAME = db_settings.get("name", DB_NAME)
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
@@ -29,8 +22,10 @@ engine = create_engine(DATABASE_URL)
 
 def test_db_connection():
     try:
-        with engine.connect():
-            return True
+        with engine.connect() as connection:
+            connection.execute(text("SELECT 1"))
+        return True
+
     except Exception as e:
         print(f"Database connection error: {str(e)}")
         return False
