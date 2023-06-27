53e1e90c26e91e354cb04d46afab7cda5ebf8bd7
Drilon Sylejmani <drilon.sylejmani2@gmail.com>
2023-06-27 09:56:57 +0200

    Added filters in the data router, filter by name and date

--- app/routers/telegrafi.py
+++ app/routers/telegrafi.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter
 from app.models.telegrafi import Article
 from app.scrapers.telegrafi import TelegrafiScraper
 from app.utils.database import session
+from datetime import datetime, timedelta
+from sqlalchemy import func
+
 
 router = APIRouter(prefix="/telegrafi")
 
@@ -26,8 +29,24 @@ async def scrape_telegrafi(url_path: str, page_numbers: int):
 
 
 @router.get("/data")
-async def telegrafi_data():
+async def telegrafi_data(
+    title_contains: str = None, date_from: str = None, date_to: str = None
+):
     articles = session.query(Article)
+
+    if title_contains:
+        articles = articles.filter(
+            func.lower(Article.name).like(func.lower(f"%{title_contains}%"))
+        )
+
+    if date_from and date_to:
+        try:
+            date_from = datetime.strptime(date_from, "%Y-%m-%d")
+            date_to = datetime.strptime(date_to, "%Y-%m-%d") + timedelta(days=1)
+            articles = articles.filter(Article.date_posted.between(date_from, date_to))
+        except ValueError:
+            return {"error": "Invalid date format. Please use YYYY-MM-DD."}
+
     results = []
     for article in articles:
         results.append(article.__dict__)
