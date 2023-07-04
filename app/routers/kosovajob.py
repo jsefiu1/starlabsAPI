23f9e7ebb86a9c452fddccab898478e5b9fe330e
arlinndi9 <arlinndi9@gmail.com>
2023-07-04 17:05:42 +0200

    implement template

--- app/routers/kosovajob.py
+++ app/routers/kosovajob.py
@@ -3,11 +3,13 @@ from sqlalchemy import func
 from app.utils.database import session
 from app.models.kosovajob import Job
 from app.scrapers.kosovajob import KosovajobScraper
-
-
+import requests
+from fastapi.templating import Jinja2Templates
+from typing import Optional
+from math import ceil
 router=APIRouter(
-    prefix='/kosovajobs',
-)
+    prefix='/kosovajobs',)
+templates = Jinja2Templates(directory="app/templates")
 
 
 @router.get('/scrape')
@@ -18,12 +20,78 @@ def scrape_and_insert(url_path: str):
     return results
 
 @router.get('/data')
-def get_data(title: str = Query(None),city: str = Query(None)):
-    if title:
+def kosovajob_data(title: str = Query(None),city: str = Query(None)):
+    if title and city:
+        jobs = session.query(Job).filter(func.lower(Job.title).contains(title.lower()), func.lower(Job.city).contains(city.lower())).all()
+    elif title:
         jobs = session.query(Job).filter(func.lower(Job.title).contains(title.lower())).all()
-    if city:
+    elif city:
         jobs = session.query(Job).filter(func.lower(Job.city).contains(city.lower())).all()
-    if not title and not city:
+    else:
         jobs = session.query(Job).all()
     return jobs
+
+
+@router.get('/view')
+def dashboard(
+    request: Request,
+    jobtitle: Optional[str] ='',
+    city: Optional[str] = '',
+    page: int = Query(1, ge=1), 
+    page_size: int = Query(10, ge=1, le=100), 
+):
+    query = session.query(Job)
+    if jobtitle and city:
+        query = query.filter(func.lower(Job.title).contains(jobtitle.lower()), func.lower(Job.city).contains(city.lower()))
+    elif jobtitle:
+        query = query.filter(func.lower(Job.title).contains(jobtitle.lower()))
+    elif city:
+        query = query.filter(func.lower(Job.city).contains(city.lower()))
+
+    
+    total_jobs = query.count()
+    total_pages = ceil(total_jobs / page_size)
+
+    max_visible_pages = 5
+    buffer_pages = max_visible_pages - 2
+
+    if total_pages <= max_visible_pages:
+        start_page = 1
+        end_page = total_pages
+    else:
+        if page <= buffer_pages:
+            start_page = 1
+            end_page = max_visible_pages - 1
+        elif page > total_pages - buffer_pages:
+            start_page = total_pages - max_visible_pages + 2
+            end_page = total_pages
+        else:
+            start_page = page - (buffer_pages // 2)
+            end_page = page + (buffer_pages // 2)
+
+    offset = (page - 1) * page_size
+    query = query.offset(offset).limit(page_size)
+    jobs = query.all()
+
+    return templates.TemplateResponse('kosovajob.html', {
+        'request': request,
+        'jobs': jobs,
+        'jobtitle': jobtitle,
+        'city': city,
+        'page': page,
+        'page_size': page_size,
+        'total_pages': total_pages,
+        'start_page': start_page,
+        'end_page': end_page,
+    })
+
+
+
+
+
+
+
+
+
+
 
--- app/scrapers/kosovajob.py
+++ app/scrapers/kosovajob.py
@@ -4,7 +4,7 @@ import requests
 import logging
 from app.models.kosovajob import Job
 from app.utils.database import session
-
+from datetime import date
 
 
 class KosovajobScraper(Scraper):
@@ -12,16 +12,20 @@ class KosovajobScraper(Scraper):
         super().__init__(base_url)
 
     def scrape(self, url_path: str):
+        current_date = date.today()
+        date_of_scrape = current_date.strftime("%d/%m/%Y")
         results = []
         response = requests.get(url_path)
         soup = BeautifulSoup(response.content, "html.parser")
 
-        parent_divs = soup.find_all('div', class_='jobListCntsInner')
+        parent_divs = soup.find_all('div', class_='jobListCnts')
         for parent_div in parent_divs:
+            image_div = parent_div.find('div', class_='jobListImage')
+            image_url = image_div['data-background-image']
             job_title = parent_div.find('div', class_='jobListTitle').text
             city = parent_div.find('div', class_='jobListCity').text
-            expire_date = parent_div.find('div', class_='jobListExpires').text
-            results.append({'title': job_title, 'city': city, 'expire_date': expire_date})
+            expires_date = parent_div.find('div', class_='jobListExpires').text
+            results.append({'image_url': image_url,'title': job_title, 'city': city, 'expires_date': expires_date, 'date_of_scrape': date_of_scrape})
 
         if results:
             return results
@@ -35,9 +39,11 @@ class KosovajobScraper(Scraper):
 
         for result in results:
             job = Job(
+                image_url=result['image_url'],
                 title=result['title'],
                 city=result['city'],
-                expire_date=result['expire_date']
+                expires_date=result['expires_date'],
+                date_of_scrape=result['date_of_scrape']
             )
             session.add(job)
 
