c551ae0730aa0b14f095272be311fdc916641308
Jon Sefiu <jonisefiu1@gmail.com>
2023-07-05 16:58:24 -0700

    Scrape gjirafa every 6 hour

--- manage.py
+++ manage.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
-from app.utils.database import test_db_connection
-import uvicorn
-from app.routers import telegrafi, gjirafa,kosovajob,home
+from app.utils.database import test_db_connection, engine, DATABASE_URL
+from app.routers import telegrafi, gjirafa, kosovajob, home
 from app.models import Base
-from app.utils.database import engine
+import uvicorn
+from fastapi_amis_admin.admin.settings import Settings
+from fastapi_amis_admin.admin.site import AdminSite
+from fastapi_scheduler import SchedulerAdmin
+from app.tasks.gjirafa import gjirafa1
+
+
 
 app = FastAPI()
 
+site = AdminSite(settings=Settings(database_url=DATABASE_URL))
+scheduler = SchedulerAdmin.bind(site)
+site.mount_app(app)
 
 @app.get("/test-db")
 def test_database_connection():
     return test_db_connection()
 
 app.include_router(home.router)
+
+@app.on_event("startup")
+async def startup():
+    scheduler.start()
 
 app.include_router(gjirafa.router)
 
