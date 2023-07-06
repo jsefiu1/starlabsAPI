f18fc323bd3464a9e24efad04a47daadca444a5e
Alzelida Kryeziu <alzelide.k@gmail.com>
2023-07-06 11:25:43 +0200

    WIP

---
+++ app/models/douglas.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from app.utils.database import engine
+from app.models import Base
+
+class Brand(Base):
+    __tablename__ = "brands"
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+    category = Column(String(200))
+    price = Column(String(200))
+    details_link = Column(String(500))
