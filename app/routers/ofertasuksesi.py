92ecc125c0eab6b1307824be9923cc9805401ee4
raonar.vokshi <raonar.vokshi@gmail.com>
2023-07-20 13:41:28 +0200

    Removed comments

--- app/routers/ofertasuksesi.py
+++ app/routers/ofertasuksesi.py
@@ -30,37 +30,6 @@ def get_ofertasuksesi_html(request: Request, page: Optional[int] = Query(1, ge=1
     rendered_html = template.render(offers=current_offers, current_page=page, pagination_numbers=range(1, total_pages + 1), total_pages=total_pages)
 
     return rendered_html
-    
-# @router.get("/search", response_class=HTMLResponse)
-# def search_offers(query: Optional[str], request: Request, page: Optional[int] = Query(1, ge=1), current_page: Optional[int] = Query(default=1, ge=1)):
-#     format_query = query.title()
-#     items_per_page = 10
-#     check_offers = session.query(Data).filter(Data.location.contains(format_query)).all()
-#     total_items = len(check_offers)
-#     total_pages = ceil(total_items / items_per_page)
-
-#     if query and not check_offers:
-#         template = templates.get_template("ofertasuksesi.html")
-#         rendered_html = template.render(offers=[], current_page=1, pagination_numbers=range(1, 2), total_pages=1, error_message="Location not found")
-
-#         return rendered_html + "Location not found!"
-
-#     if not check_offers:
-#         template = templates.get_template("ofertasuksesi.html")
-#         rendered_html = template.render(offers=[], current_page=1, pagination_numbers=range(1, 2), total_pages=1, error_message="Location not found")
-#         return rendered_html + "Data not found!"
-
-#     if query:
-#         items_per_page = 10
-
-#     start_index = (page - 1) * items_per_page
-#     end_index = start_index + items_per_page
-#     current_offers = check_offers[start_index:end_index]
-
-#     template = templates.get_template("ofertasuksesi.html")
-#     rendered_html = template.render(offers=current_offers, current_page=page, pagination_numbers=range(1, total_pages + 1), total_pages=total_pages)
-
-#     return rendered_html
 
 @router.get("/search", response_class=HTMLResponse)
 def search_offers(
@@ -110,7 +79,7 @@ def search_offers(
         current_page=page,
         pagination_numbers=range(1, total_pages + 1),
         total_pages=total_pages,
-        query=query  # Pass the query parameter to the template
+        query=query
     )
 
     return rendered_html
@@ -146,20 +115,3 @@ def get_ofertasuksesi_data(limit: int = None, offset: int = None, title: str = N
         results.append(offer.__dict__)
     
     return {"results": results, "total_pages": total_pages}
-
-    
-
-
-    # if not offers:
-    #     raise HTTPException(status_code=404, detail="No data in database")
-    # else:
-    #     return offers
-
-# @router.get("/ofertasuksesi/by-title-location")
-# def get_ofertasuksesi_by_title_location(title: str = Query(None), location: str = Query(None)):
-#     format_loc = location.title().replace(" ", "")
-#     offer = session.query(Data).filter(Data.title == title, Data.location == format_loc).first()
-#     if offer is None:
-#         raise HTTPException(status_code=404, detail="Title or location not found")
-#     else:
-#         return offer
