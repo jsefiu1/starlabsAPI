9560363ce6425fe8b1c501cb38cb144ac49e6d1a
Jon <jon.fejzullahu.jon@gmail.com>
2023-08-30 17:21:01 +0200

    user_reviews

--- app/models/auth.py
+++ app/models/auth.py
@@ -50,3 +50,12 @@ class Contact(Base):
     name = Column(String)
     email = Column(String)
     message = Column(String)
+
+class Review(Base):
+    __tablename__ = "review"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    content = Column(String)
+    user = Column(String)
+    rating = Column(Integer)
